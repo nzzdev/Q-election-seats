1b6a15fc356f3969503b123b4cbc13c4ed35394e
Philip Küng <kueng.philip@gmail.com>
2019-01-16 15:48:12 +0100

    added e2e-tests

---
+++ test/e2e-tests.js
@@ -0,0 +1,130 @@
+const Lab = require("lab");
+const Code = require("code");
+const Hapi = require("hapi");
+const lab = (exports.lab = Lab.script());
+
+const expect = Code.expect;
+const before = lab.before;
+const after = lab.after;
+const it = lab.it;
+
+const routes = require("../routes/routes.js");
+const plugins = require("../server-plugins.js");
+
+let server;
+
+before(async () => {
+  try {
+    server = Hapi.server({
+      port: process.env.PORT || 3000,
+      routes: {
+        cors: true
+      }
+    });
+    server.register(plugins);
+    server.route(routes);
+  } catch (err) {
+    expect(err).to.not.exist();
+  }
+});
+
+after(async () => {
+  await server.stop({ timeout: 2000 });
+  server = null;
+});
+
+lab.experiment("basics", () => {
+  it("starts the server", () => {
+    expect(server.info.created).to.be.a.number();
+  });
+
+  it("is healthy", async () => {
+    const response = await server.inject("/health");
+    expect(response.payload).to.equal("ok");
+  });
+});
+
+lab.experiment("schema endpoint", () => {
+  it("returns 200 for /schema.json", async () => {
+    const response = await server.inject("/schema.json");
+    expect(response.statusCode).to.be.equal(200);
+  });
+});
+
+lab.experiment("locales endpoint", () => {
+  it("returns 200 for en translations", async () => {
+    const request = {
+      method: "GET",
+      url: "/locales/en/translation.json"
+    };
+    const response = await server.inject(request);
+    expect(response.statusCode).to.be.equal(200);
+  });
+  it("returns 200 for fr translations", async () => {
+    const request = {
+      method: "GET",
+      url: "/locales/fr/translation.json"
+    };
+    const response = await server.inject(request);
+    expect(response.statusCode).to.be.equal(200);
+  });
+});
+
+lab.experiment("stylesheets endpoint", () => {
+  it(
+    "returns existing stylesheet with right cache control header",
+    { plan: 2 },
+    async () => {
+      const filename = require("../styles/hashMap.json").default;
+      const response = await server.inject(`/stylesheet/${filename}`);
+      expect(response.statusCode).to.be.equal(200);
+      expect(response.headers["cache-control"]).to.be.equal(
+        "max-age=31536000, immutable"
+      );
+    }
+  );
+
+  it("returns Not Found when requesting an inexisting stylesheet", async () => {
+    const response = await server.inject("/stylesheet/inexisting.123.css");
+    expect(response.statusCode).to.be.equal(404);
+  });
+});
+
+lab.experiment("rendering-info endpoint", () => {
+  it("returns 200 for /rendering-info/html-static", async () => {
+    const request = {
+      method: "POST",
+      url: "/rendering-info/html-static",
+      payload: {
+        item: require("../resources/fixtures/data/results-long-names-vacancy-zero-seats"),
+        toolRuntimeConfig: {
+          displayOptions: {}
+        }
+      }
+    };
+    const response = await server.inject(request);
+    expect(response.statusCode).to.be.equal(200);
+  });
+});
+
+lab.experiment("migration endpoint", () => {
+  it("returns 304 for /migration", async () => {
+    const request = {
+      method: "POST",
+      url: "/migration",
+      payload: {
+        item: require("../resources/fixtures/data/results-long-names-vacancy-zero-seats")
+      }
+    };
+    const response = await server.inject(request);
+    expect(response.statusCode).to.be.equal(304);
+  });
+});
+
+lab.experiment("fixture data endpoint", () => {
+  it("returns 4 fixture data items for /fixtures/data", async () => {
+    const response = await server.inject("/fixtures/data");
+    expect(response.statusCode).to.be.equal(200);
+    expect(response.result.length).to.be.equal(4);
+  });
+});
