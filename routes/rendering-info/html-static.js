326596bccaf51f854c7633d87bd7b9b75b39638c
Philip Küng <kueng.philip@gmail.com>
2019-01-16 15:47:50 +0100

    implement new version of jsdom

--- routes/rendering-info/html-static.js
+++ routes/rendering-info/html-static.js
@@ -25,76 +25,69 @@ const displayOptionsSchema = Enjoi(
 require("svelte/ssr/register");
 const staticTemplate = require(viewsDir + "HtmlStatic.html");
 
-var jsdom = require("jsdom");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 var d3 = require("d3");
 
 function getMarkupWithSeatSvg(parties, markup, width) {
   return new Promise((resolve, reject) => {
-    jsdom.env({
-      html: markup,
-      features: {
-        QuerySelector: true // query selector is needed for d3 to work
-      },
-      done: (errors, window) => {
-        if (errors) {
-          reject(errors);
-          return;
-        }
-        let height = width / 2;
-        let radius = width / 2;
-        let svgContainerElement = window.document.getElementById(
-          "q-election-seats-svg-container"
-        );
+    try {
+      const dom = new JSDOM(markup);
+      let height = width / 2;
+      let radius = width / 2;
+      let svgContainerElement = dom.window.document.getElementById(
+        "q-election-seats-svg-container"
+      );
+      if (svgContainerElement) {
+        let svg = d3
+          .select(svgContainerElement)
+          .append("svg")
+          .attr("viewbox", "0 0 " + width + " " + height)
+          .attr("class", "q-election-seats-svg-content")
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height + ")");
 
-        if (svgContainerElement) {
-          let svg = d3
-            .select(svgContainerElement)
-            .append("svg")
-            .attr("viewbox", "0 0 " + width + " " + height)
-            .attr("class", "q-election-seats-svg-content")
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height + ")");
+        let arc = d3
+          .arc()
+          .innerRadius(radius / 3)
+          .outerRadius(radius);
 
-          let arc = d3
-            .arc()
-            .innerRadius(radius / 3)
-            .outerRadius(radius);
+        let pie = d3
+          .pie()
+          .value(party => {
+            return party.seats;
+          })
+          .sort(null)
+          .startAngle(-90 * (Math.PI / 180))
+          .endAngle(90 * (Math.PI / 180));
 
-          let pie = d3
-            .pie()
-            .value(party => {
-              return party.seats;
-            })
-            .sort(null)
-            .startAngle(-90 * (Math.PI / 180))
-            .endAngle(90 * (Math.PI / 180));
-
-          let path = svg
-            .selectAll("path")
-            .data(pie(parties))
-            .enter()
-            .append("path")
-            .attr("class", parties => {
-              if (parties.data.color && parties.data.color.classAttribute) {
-                return parties.data.color.classAttribute;
-              } else {
-                return "";
-              }
-            })
-            .attr("d", arc)
-            .attr("stroke", "#f5f5f5")
-            .attr("fill", parties => {
-              let color = parties.data.color;
-              if (color && !color.classAttribute) {
-                return color.colorCode;
-              } else {
-                return "currentColor";
-              }
-            });
-        }
-        resolve(window.document.body.innerHTML);
+        let path = svg
+          .selectAll("path")
+          .data(pie(parties))
+          .enter()
+          .append("path")
+          .attr("class", parties => {
+            if (parties.data.color && parties.data.color.classAttribute) {
+              return parties.data.color.classAttribute;
+            } else {
+              return "";
+            }
+          })
+          .attr("d", arc)
+          .attr("stroke", "#f5f5f5")
+          .attr("fill", parties => {
+            let color = parties.data.color;
+            if (color && !color.classAttribute) {
+              return color.colorCode;
+            } else {
+              return "currentColor";
+            }
+          });
       }
-    });
+      resolve(dom.window.document.body.innerHTML);
+    } catch (ex) {
+      reject(ex);
+    }
   });
 }
 
