9688f210afd2bf18c9ef30544bd2b10e77103866
Philip Küng <kueng.philip@gmail.com>
2019-02-21 15:05:30 +0100

    update to enjoi 5.0 conventions

--- routes/rendering-info/html-static.js
+++ routes/rendering-info/html-static.js
@@ -12,15 +12,7 @@ const schemaString = JSON.parse(
     encoding: "utf-8"
   })
 );
-const schema = Enjoi(schemaString);
-
-const displayOptionsSchema = Enjoi(
-  JSON.parse(
-    fs.readFileSync(resourcesDir + "display-options-schema.json", {
-      encoding: "utf-8"
-    })
-  )
-);
+const schema = Enjoi.schema(schemaString);
 
 require("svelte/ssr/register");
 const staticTemplate = require(viewsDir + "HtmlStatic.html");
