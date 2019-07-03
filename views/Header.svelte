3b50715d5d92adfca0ef9c6c015d25a986da77ec
Philip Küng <kueng.philip@gmail.com>
2019-07-03 13:57:51 +0200

    migrated project to svelte3

---
+++ views/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let title;
+  export let subtitle;
+  export let hideTitle;
+</script>
+
+{#if hideTitle !== true}
+  <h3 class="s-q-item__title">{title}</h3>
+{/if}
+<div class="s-q-item__subtitle">
+  {#if subtitle}{subtitle}{/if}
+</div>
